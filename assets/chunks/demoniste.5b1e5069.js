619f7ce6f1a1236c824d7060f786b5b0966aefb5
Stephane-Pires <Stephane-Pires@users.noreply.github.com>
2023-01-24 23:50:03 +0000

    deploy: 71b9c83807ab8e633be9c767abab26aee4525fb7

---
+++ assets/chunks/demoniste.5b1e5069.js
@@ -0,0 +1,1 @@
+const s="/lootosaurus/classes/rogue.png",o="/lootosaurus/classes/druide.png",t="/lootosaurus/classes/demoniste.png";export{s as _,o as a,t as b};
---
+++ assets/chunks/paladin.3be07437.js
@@ -0,0 +1,1 @@
+const s="/lootosaurus/classes/shaman.png",o="/lootosaurus/classes/mage.png",a="/lootosaurus/classes/hunter.png",t="/lootosaurus/classes/priest.png",n="/lootosaurus/classes/dk.png",p="/lootosaurus/classes/paladin.png";export{s as _,o as a,a as b,t as c,n as d,p as e};
